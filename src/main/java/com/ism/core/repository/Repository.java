20c5291e72092f82943a09cb9797e93e2f9ea69f
Otra10 <traoreopo015@gmail.com>
2024-10-30 09:16:50 +0000

    mise à jour

--- src/main/java/com/ism/core/repository/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.ism.core.repository;
-
-import java.util.List;
-
-public interface Repository<T> {
-    void insert(T data);
-    List<T> selectAll();
-    
-}
--- src/main/java/com/ism/data/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ism.data.repository;
-
-import com.ism.core.repository.Repository;
-import com.ism.data.entities.User;
-
-public interface UserRepository extends Repository<User> {
-    User selectByLogin(String login);
-    User selectByID(int id);
-}
